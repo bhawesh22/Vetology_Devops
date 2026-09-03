d78105b1894c8823c5ed10f802fe66d0a64fedb4
bhawesh22 <bhawesh.mishra@kapilshrivastavaG20outlook.onmicrosoft.com>
2026-09-03 13:09:13 +0530

    update app code

--- terraform/Dev/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vm_public_ip" {
-  description = "Public IP address of the DevOps VM"
-  value       = module.compute.vm_public_ip
-}
--- terraform/modules/compute/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vm_public_ip" {
-  description = "Public IP address of the VM"
-  value       = azurerm_public_ip.pip.ip_address
-}
